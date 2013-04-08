a9102bf957efeb9099348e6d2c540423a3888bac
David Cheng <davidxcheng@gmail.com>
2013-04-09 00:06:28 +0200

    express + jade + stylus

---
+++ app.js
@@ -0,0 +1,46 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
+// setup stylus with custom compile
+var stylus = require('stylus');
+function compile(str, path) {
+	return stylus(str).define('url', stylus.url({ limit: 1000000 }));	
+}
+
+app.use(stylus.middleware({ 
+	src: 		__dirname + '/public',
+	compile: 	compile
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
