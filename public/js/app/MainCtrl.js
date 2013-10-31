0932b96ad24bc09366cc9b48adb872582fe2460b
David Cheng <davidxcheng@gmail.com>
2013-10-31 23:45:32 +0100

    cleaning up

--- public/js/app/MainCtrl.js
+++ public/js/app/MainCtrl.js
@@ -1,10 +1,17 @@
-define(['libs/angularjs.1.0.6.min'], function(ng) {
+define(['libs/angularjs.1.0.6.min', 'libs/jQuery.1.9.1.min'], function(ng, $) {
+	console.log('bb');
 
 	var refreshMenu = function() {
 		$.get('/menu', function(menuItems) {
 			$('#main-menu').html(menuItems);
 		});
 	};
+
+	$('#main-menu a').on('click', function(e) {
+		$('#main-menu .active').removeClass('active');
+		console.log("fdfsdf");
+		$(this).addClass('active');
+	});
 
 	return function($scope, $route, $routeParams, $location, $http) {
 
