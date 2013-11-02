d4d06b312ebaded74f6e749403a68f51b3700850
David Cheng <davidxcheng@gmail.com>
2013-11-02 20:16:28 +0100

    config changes made (in vain) to get trhough deploy at nodejitsu

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
 	port: 3000,
 	mongoDb: {
-		connectionString: 'mongodb://localhost:27017/tt3k',
+		//connectionString: 'mongodb://localhost:27017/tt3k',
+		connectionString: 'mongodb://nodejitsu_chevycheng:jhk1d4pf2e6mv95lfdc99dg4l3@ds045988.mongolab.com:45988/nodejitsu_chevycheng_nodejitsudb5574478292',
 		collections: {
 			Members: 'Members',
 			Scores: 'Scores'
