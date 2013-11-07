40aebbca0b405d3c8cf41a6e741ea706db511b8b
David Cheng <davidxcheng@gmail.com>
2013-11-07 01:17:22 +0100

    began validation of score

--- public/js/app/LatestScoresCtrl.js
+++ public/js/app/LatestScoresCtrl.js
@@ -6,6 +6,7 @@ define(['libs/angularjs.1.0.6.min', 'libs/jquery.1.9.1.min'], function(ng, $) {
 		$http.get('/scores/latest')
 			.success(function(scores) {
 				$scope.scores = scores;
+				$('#main-menu .active').removeClass('active');
 				$('#root').addClass('active');
 			});
 	};
--- public/js/app/ScoresCtrl.js
+++ public/js/app/ScoresCtrl.js
@@ -1,12 +1,46 @@
 define(['libs/angularjs.1.0.6.min', 'libs/bootstrap-datepicker'], function(ng) {
 
 	return function($scope, $http, $location) {
+
 		$('#when').datepicker({ format: 'yyyy-mm-dd' });
+
+		$scope.match = {
+			gameday: '',
+			player1: '',
+			player2: '',
+			sets: [
+				{
+					set: 1,
+					gamesWonByPlayer1: null,
+					gamesWonByPlayer2: null,
+				},
+				{
+					set: 2,
+					gamesWonByPlayer1: null,
+					gamesWonByPlayer2: null,
+				},
+				{
+					set: 3,
+					gamesWonByPlayer1: null,
+					gamesWonByPlayer2: null,
+				},
+				{
+					set: 4,
+					gamesWonByPlayer1: null,
+					gamesWonByPlayer2: null,
+				},
+				{
+					set: 5,
+					gamesWonByPlayer1: null,
+					gamesWonByPlayer2: null,
+				}
+			]
+		};
 
 		$http.get('/member/current')
 			.success(function(user, status) {
 				if (user) {
-					$scope.player1 = user.name;
+					$scope.match.player1 = user.name;
 					$http.get('/players')
 						.success(function(data, status) {
 							$scope.opponents = data;
@@ -16,41 +50,40 @@ define(['libs/angularjs.1.0.6.min', 'libs/bootstrap-datepicker'], function(ng) {
 					$location.path('/login');
 			});
 
-		$scope.submitScore = function() {
-			var match = {
-				gameday: $('#when').val(),
-				player1: $('.player').html(),
-				player2: $('#player2').val(),
-				sets: [
-					{
-						set: 1,
-						gamesWonByPlayer1: $('#set1-player1').val(),
-						gamesWonByPlayer2: $('#set1-player2').val(),
-					},
-					{
-						set: 2,
-						gamesWonByPlayer1: $('#set2-player1').val(),
-						gamesWonByPlayer2: $('#set2-player2').val(),
-					},
-					{
-						set: 3,
-						gamesWonByPlayer1: $('#set3-player1').val(),
-						gamesWonByPlayer2: $('#set3-player2').val(),
-					},
-					{
-						set: 4,
-						gamesWonByPlayer1: $('#set4-player1').val(),
-						gamesWonByPlayer2: $('#set4-player2').val(),
-					},
-					{
-						set: 5,
-						gamesWonByPlayer1: $('#set5-player1').val(),
-						gamesWonByPlayer2: $('#set5-player2').val(),
-					}
-				]
-			};
+		$scope.validate = function(match) {
 
-			$http.post('/scores', match)
+			var isInvalid = false;
+
+			if (!match.player2) {
+				isInvalid = true;
+			}
+
+			if (!match.sets[0].gamesWonByPlayer1) {
+				isInvalid = true;
+			}
+
+			if (!match.sets[0].gamesWonByPlayer2) {
+				isInvalid = true;
+			}
+
+			return {
+				failed: isInvalid,
+				message: ":'("
+			};
+		}
+
+		$scope.submitScore = function() {
+
+			$scope.match.gameday = $('#when').val();
+
+			var validation = $scope.validate($scope.match);
+			if (validation.failed) {
+				$scope.feedback = validation.message;
+				$('#feedback').removeClass('hide');
+				return false;
+			}
+
+			$http.post('/scores', $scope.match)
 				.success(function() {
 					$location.path('/');
 				});
